774e50f6054c58aea51fb77bb36ebf7fab94053c
nikunj <nikunjlap@gmail.com>
2022-02-20 19:28:03 +0530

    quiz app initialized and on last state before ready

---
+++ quizapp/quiz/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Quizzes
+from .serializers import QuizSerializer
+# Create your views here.
+
+
+class Quiz(generics.ListAPIView):
+	serializer_class = QuizSerializer
+	queryset = Quizzes.objects.all()
